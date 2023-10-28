e9d0b8cdcd9e39b5aa8c912903dc92b54e444340
nourbenamor0001 <146487504+nourbenamor0001@users.noreply.github.com>
2023-10-28 07:49:10 +0000

    Pending changes exported from your codespace

---
+++ web/src/App.js
@@ -0,0 +1,27 @@
+import ImageSlider from "./Components/ImageSlider";
+
+function App() {
+const slides = [
+  { url: "http://localhost:3000/image-1.jpg", title: "beach" },
+    { url: "http://localhost:3000/image-2.jpg", title: "boat" },
+    { url: "http://localhost:3000/image-3.jpg", title: "forest" },
+    { url: "http://localhost:3000/image-4.jpg", title: "city" },
+    { url: "http://localhost:3000/image-5.jpg", title: "italy" },
+]; 
+
+const containerStyles = {
+  width:'500px',
+  height:"280px",
+  margin:"0 auto",
+};
+
+  return (
+  <div>  
+    <div style={containerStyles}>
+      <ImageSlider slides={slides}/>
+    </div>
+  </div>
+  );
+};
+
+export default App;
